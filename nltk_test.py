34eea5b7942f7e2db3db06260f66461b1bf9416d
涂松高 <tusonggao@163.com>
2018-12-03 10:54:31 +0800

    update nltk_test.py

--- nltk_test.py
+++ nltk_test.py
@@ -1,3 +1,4 @@
+import time
 import nltk
 # import gensim
 # import spacy
@@ -6,8 +7,30 @@ import pandas as pd
 
 print(nltk.__version__)
 
-sentence = """At eight o'clock on Thursday morning 
-     Arthur didn't feel very good."""
+start_t = time.time()
+
+# nltk.download('punkt')
+# nltk.download('averaged_perceptron_tagger')
+# nltk.download('maxent_ne_chunker')
+# nltk.download('words')
+nltk.download('treebank')
+
+print('download cost time', time.time()-start_t)
+
+sentence = """At eight o'clock on Thursday sunny morning 
+     Arthur didn't feel very good. he said: I won't give up, never"""
 tokens = nltk.word_tokenize(sentence)
 
 print(tokens)
+
+tagged = nltk.pos_tag(tokens)
+
+print(tagged)
+
+entities = nltk.chunk.ne_chunk(tagged)
+
+print(entities)
+
+from nltk.corpus import treebank
+t = treebank.parsed_sents('wsj_0001.mrg')[0]
+t.draw()
