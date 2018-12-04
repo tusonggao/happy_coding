e06eadd323b66d3b6735a409c21138f9cf0fd1af
涂松高 <tusonggao@163.com>
2018-12-04 17:43:50 +0800

    check the corpora

--- nltk_test.py
+++ nltk_test.py
@@ -14,7 +14,7 @@ start_t = time.time()
 # nltk.download('maxent_ne_chunker')
 # nltk.download('words')
 # nltk.download('treebank')
-nltk.download()
+# nltk.download()
 
 print('download cost time', time.time()-start_t)
 
@@ -32,6 +32,8 @@ entities = nltk.chunk.ne_chunk(tagged)
 
 print(entities)
 
-from nltk.corpus import treebank
-t = treebank.parsed_sents('wsj_0003.mrg')[0]
-t.draw()
+# from nltk.corpus import treebank
+# t = treebank.parsed_sents('wsj_0003.mrg')[0]
+# t.draw()
+
+print(nltk.corpus.gutenberg.fileids())
