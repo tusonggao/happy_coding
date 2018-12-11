380fef13eac2a1d68a70acc2704fcac946563870
tusonggao <tusonggao@163.com>
2018-12-11 20:18:53 +0800

    update wide and deep

--- tf_wide_and_deep.py
+++ tf_wide_and_deep.py
@@ -14,3 +14,37 @@ import pandas as pd
 import tensorflow as tf
 
 print(tf.__version__)
+
+COLUMNS = ["age", "workclass", "fnlwgt", "education", "education_num",
+           "marital_status", "occupation", "relationship", "race", "gender",
+           "capital_gain", "capital_loss", "hours_per_week", "native_country",
+           "income_bracket"]
+LABEL_COLUMN = "label"
+CATEGORICAL_COLUMNS = ["workclass", "education", "marital_status", "occupation",
+                       "relationship", "race", "gender", "native_country"]
+CONTINUOUS_COLUMNS = ["age", "education_num", "capital_gain", "capital_loss",
+                      "hours_per_week"]
+
+def maybe_download(train_data, test_data):
+  """Maybe downloads training data and returns train and test file names."""
+  if train_data:
+    train_file_name = train_data
+  else:
+    train_file = open('train.data', 'wb')
+    urllib.request.urlretrieve("http://mlr.cs.umass.edu/ml/machine-learning-databases/adult/adult.data", train_file.name)  # pylint: disable=line-too-long
+    train_file_name = train_file.name
+    train_file.close()
+    print("Training data is downloaded to %s" % train_file_name)
+
+  if test_data:
+    test_file_name = test_data
+  else:
+    test_file = open('test.data', 'wb')
+    urllib.request.urlretrieve("http://mlr.cs.umass.edu/ml/machine-learning-databases/adult/adult.test", test_file.name)  # pylint: disable=line-too-long
+    test_file_name = test_file.name
+    test_file.close()
+    print("Test data is downloaded to %s" % test_file_name)
+
+  return train_file_name, test_file_name
+
+train_file_name, test_file_name = maybe_download("train.data", "test.data")
